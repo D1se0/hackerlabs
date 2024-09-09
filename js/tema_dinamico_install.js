dc5848fcdb644a1b24c264b28f2f7bd1de7b04b6
Diseo <164921056+D1se0@users.noreply.github.com>
2024-09-09 08:57:28 +0200

    Add files via upload

---
+++ js/tema_dinamico_install.js
@@ -0,0 +1,38 @@
+// script.js
+const button = document.getElementById('toggleButton');
+const slider = document.querySelector('.slider');
+const themeStylesheet = document.getElementById('theme-stylesheet');
+const body = document.body;
+
+// Ajustes para la posición del slider
+const sliderPositionLight = 'translateX(0)'; // Posición para modo claro
+const sliderPositionDark = 'translateX(75px)'; // Posición ajustada para modo oscuro
+
+// Cargar el tema guardado si existe
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-mode');
+    themeStylesheet.setAttribute('href', 'css/style_dark_install.css');
+    slider.style.transform = sliderPositionDark;
+} else {
+    body.classList.add('light-mode');
+    themeStylesheet.setAttribute('href', 'css/style_light_install.css');
+    slider.style.transform = sliderPositionLight;
+}
+
+// Cambiar el tema al hacer clic
+button.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        themeStylesheet.setAttribute('href', 'css/style_light_install.css');
+        slider.style.transform = sliderPositionLight;
+        localStorage.setItem('theme', 'light');
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        themeStylesheet.setAttribute('href', 'css/style_dark_install.css');
+        slider.style.transform = sliderPositionDark;
+        localStorage.setItem('theme', 'dark');
+    }
+});
